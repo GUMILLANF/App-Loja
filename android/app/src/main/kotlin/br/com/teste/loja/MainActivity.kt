12469a2200a21dbf2d4f123c959f54691d335d46
GUMILLANF <gumillanf@gmail.com>
2023-11-08 21:38:37 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/teste/loja/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.teste.loja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const userFavoritesUrl =
+      'https://shop-4b7c6-default-rtdb.firebaseio.com/userFavorites';
+  static const productBaseUrl =
+      'https://shop-4b7c6-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-4b7c6-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyAx94Gq4sP24C9nlKzFeOCEqj1SRL6uDcQ';
+}
